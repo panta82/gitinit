06548c43d5ccaec15ecc380ee8b67938e1f3f85a
Ivan Pantic <ivan@pantas.net>
2021-09-30 10:52:06 +0200

    Fix problems with generating ts node app

--- gitinit.sh
+++ gitinit.sh
@@ -234,6 +234,7 @@ exec_ts_app() {
   cp -r "${DIR}/assets/ts_app/src" "./src"
 
   log "Setting up logo..."
+  mkdir './src/assets/'
   echo -e "\n\n${NAME}" > './src/assets/logo-ascii.txt'
 
   log "Setting up JetBrains project..."
